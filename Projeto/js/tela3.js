b194bd995e302394fb00ccb6690438ebda77fc88
Karina Ayumi Fucuhara <karina.ayumi1@gmail.com>
2020-07-23 23:32:49 -0300

    ok

---
+++ Projeto/js/tela3.js
@@ -0,0 +1,47 @@
+function gerarRelEventos(event) {
+
+    event.preventDefault(); //não permite o envio padão do formulário
+    let dataInicio = document.getElementById("inicio").value;
+    let dataFim = document.getElementById("fim").value;
+
+    let msgLogin = {
+        email: txtUser,
+        racf: txtUser,
+        senha: txtSenha
+    }
+
+    let cabecalho = {
+        method: 'POST',
+        body: JSON.stringify(msgLogin),
+        headers: {
+            'Content-type': 'application/json'
+        }
+    }
+
+    fetch("http://localhost:8080/login", cabecalho)
+        .then(res => tratarResposta(res));
+
+}
+
+
+/* function exibirDados(res) {
+    let tabelaTxt = "<table class = 'table table-sm'> <tr> <th>Nome</th> <th>Nome</th> <th>Racf</th> </tr>"
+
+    res.forEach(user => {
+        tabelaTxt = tabelaTxt + `<tr> <td>${user.nome}</td> 
+                                    <td>${user.email}</td>
+                                    <td>${user.racf}</td></tr>`
+    });
+    tabelaTxt = tabelaTxt + "</table>";
+
+    document.getElementById("tabela").innerHTML = tabelaTxt;
+} */
+
+function testeBotao() {
+    let dataInicio = document.getElementById("inicio").value;
+    let dataFim = document.getElementById("fim").value;
+    console.log(dataInicio + " "+ dataFim);
+
+    let tabelaTxt = "<table class = 'table table-sm'> <tr> <th>Data</th> <th>Alarme</th> <th>Equipamento</th> </tr>"
+    document.getElementById("tabela").innerHTML = tabelaTxt;
+}
